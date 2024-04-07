9d80e8accf523fe9a877e07d1f74136ee1b4f892
Abbas Aghayari <Abbas2017@gmail.com>
2024-04-07 18:02:29 +0330

    deleted extra core/services files

--- projects/ngz-shamsi-datepicker/src/core/services/public-api.ts
+++ projects/ngz-shamsi-datepicker/src/core/services/public-api.ts
@@ -4,9 +4,5 @@
  */
 
 export * from './resize';
-export * from './singleton';
-export * from './drag';
-export * from './scroll';
 export * from './breakpoint';
 export * from './destroy';
-export * from './image-preload';
